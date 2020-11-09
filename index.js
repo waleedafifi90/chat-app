4d9ed2118788c549953047f1c2bd78d77f859ca0
Waleed A. Afifi <waleedafifich@gmail.com>
2020-11-09 19:46:37 +0200

    changes

--- index.js
+++ index.js
@@ -11,25 +11,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
+router.get('/', function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
+  res.send('cors problem fixed:)');
 });
-
 // app.use(cors());
 app.use(router);
 
